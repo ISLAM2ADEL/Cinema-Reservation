bb2d39f9a3371624e19aa741fe6a87372bf75dce
Mario Nady <marionadydev@gmail.com>
2026-04-27 13:34:23 +0300

    adding initial movie and seat files

---
+++ Backend-API/models/movie.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        genre: {
+            type: [String],
+            required: true,
+            enum: {
+                values: ['Action', 'Drama', 'Comedy', 'Horror', 'Thriller', 'Sci-Fi', 'Romance', 'Animation', 'Documentary', 'Adventure', 'Fantasy', 'Mystery', 'Crime',]
+            }
+        },
+        rating: {
+            type: String,
+            required: true,
+        },
+        language: {
+            type: String,
+            required: true,
+        },
+        poster: {
+            type: String,
+            required: true,
+        },
+        releaseDate: {
+            type: Date,
+            required: true,
+        },
+        isNowShowing: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const movieModel = mongoose.model("movie", movieSchema);
+
+export { movieModel };
