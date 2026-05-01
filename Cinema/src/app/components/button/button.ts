9db4cc3f87d7ddf02bb537000642cbef5913d874
MohamedEssam2127 <msam01619@gmail.com>
2026-05-01 22:04:31 +0300

    add design to button

--- Cinema/src/app/components/button/button.ts
+++ Cinema/src/app/components/button/button.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './button.html',
   styleUrl: './button.css',
+  host: { '[style.display]': '"block"' },
 })
-export class Button {}
+export class Button {
+  label = input('');
+  iconPath = input('');
+  variant = input<'primary' | 'secondary' | 'icon-only'>('primary');
+  fullWidth = input(false);
+  btnClick = output<void>();
+
+  onClick() {
+    this.btnClick.emit();
+  }
+}
